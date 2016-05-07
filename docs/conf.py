631eddbaef9c3519c0f23ac11ff990c97f2230fd
itaiag <itai.agmon@gmail.com>
2016-05-07 12:30:24 +0300

    wip

--- docs/conf.py
+++ docs/conf.py
@@ -31,7 +31,7 @@ import os
 extensions = [
     'sphinx.ext.todo',
     # 'sphinx.ext.imgmath',
-    'sphinx.ext.githubpages',
+    # 'sphinx.ext.githubpages',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
